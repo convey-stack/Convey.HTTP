3db3f44e6648b52a288fdd5dd5e8c6011861a252
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-06-28 22:25:43 +0200

    Fixed options

--- src/Convey.HTTP/Extensions.cs
+++ src/Convey.HTTP/Extensions.cs
@@ -6,7 +6,7 @@ namespace Convey.HTTP
     public static class Extensions
     {
         private const string SectionName = "httpClient";
-        private const string RegistryName = "httpClient";
+        private const string RegistryName = "http.client";
 
         public static IConveyBuilder AddHttpClient(this IConveyBuilder builder, string sectionName = SectionName)
         {
@@ -15,7 +15,7 @@ namespace Convey.HTTP
                 return builder;
             }
 
-            var options = builder.GetOptions<HttpClient>(sectionName);
+            var options = builder.GetOptions<HttpClientOptions>(sectionName);
             builder.Services.AddSingleton(options);
             builder.Services.AddHttpClient<IHttpClient, ConveyHttpClient>();
 
